e0d78e612e363c6ecf60f55d1b140bfd9e1220b2
ArraBosss <palivglazok@gmail.com>
2022-02-21 22:39:14 +0200

    16

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+  
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     onStart: function (data) {
@@ -43,7 +46,7 @@ $(function () {
         breakpoint: 1230,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 4,         
+          slidesToScroll: 4,
         }
       },
       {
@@ -63,11 +66,11 @@ $(function () {
     ]
   });
 
-  $('.product-item__list').slick({
-    
+  $('.product__list').slick({
+
     dots: false,
     arrows: true,
-    appendArrows: '.product-item__nav',
+    appendArrows: '.product__nav',
     prevArrow: '<button class="slide-arrow prev-arrow"><svg width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.384665 15.2262L16.398 0.839919C16.9234 0.324519 17.7756 0.324519 18.3011 0.839919C18.8266 1.35528 18.8266 2.19151 18.3011 2.70691L3.25336 16.2267L18.2998 29.7465C18.8253 30.2619 18.8253 31.0981 18.2998 31.6135C17.7743 32.1288 16.9221 32.1288 16.3967 31.6135L0.38333 17.2273C0.103261 16.9525 -0.0168143 16.5883 0.00189296 16.228C-0.0155212 15.8665 0.104514 15.5023 0.384665 15.2262Z" fill="#363853"/></svg></button>',
 
     nextArrow: '<button class="slide-arrow next-arrow"><svg width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.3105 14.7728L2.29726 0.38655C1.77178 -0.12885 0.919578 -0.12885 0.394105 0.38655C-0.131368 0.90191 -0.131369 1.73814 0.394104 2.25354L15.4418 15.7734L0.395433 29.2931C-0.130041 29.8085 -0.130041 30.6447 0.395432 31.1601C0.920905 31.6755 1.77311 31.6755 2.29854 31.1601L18.3119 16.7739C18.5919 16.4991 18.712 16.1349 18.6933 15.7747C18.7107 15.4131 18.5907 15.0489 18.3105 14.7728Z" fill="#363853"/></svg></button>'
@@ -103,9 +106,9 @@ $(function () {
     $('.burger, .burger__line, .overlay, .side-nav').toggleClass('active');
   }
 
-  $('.catalog__sort-filter, .product-item__num').styler();
+  $('.catalog__sort-filter, .product__num').styler();
 
-  $(".product-item__star").rateYo({
+  $(".product__star").rateYo({
     starWidth: "16px",
     spacing: "6px",
     normalFill: "#C1C1C1",
@@ -121,7 +124,7 @@ $(function () {
     fullStar: true
   });
 
-  $('.product-tabs__link').on('click', function(e){
+  $('.product-tabs__link').on('click', function (e) {
     e.preventDefault();
     $('.product-tabs__link').removeClass('product-tabs__link--active');
     $(this).addClass('product-tabs__link--active');
@@ -131,6 +134,31 @@ $(function () {
 
   })
 
+  const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
+    preload: 2,
+    Dots: false,
+  });
+  
+  // Customize Fancybox
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    Thumbs: false,
+    Toolbar: false,
+  
+    closeButton: "top",
+  
+    Carousel: {
+      Dots: true,
+      on: {
+        change: (that) => {
+          myCarousel.slideTo(myCarousel.findPageForSlide(that.page), {
+            friction: 0,
+          });
+        },
+      },
+    },
+  });
+
 });
 
 var mixer = mixitup('.popular__menu');
+
--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,8 @@ function scripts() {
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'node_modules/rateyo/src/jquery.rateyo.js',
-      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+      'node_modules/@fancyapps/ui/dist/carousel.umd.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
